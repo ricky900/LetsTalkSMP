609291e020fce1a20fd955d8ec61cb969453d9c8
RICKY900 <95167944+ricky900@users.noreply.github.com>
2022-06-04 11:51:48 +0300

    Update navbar.jsx

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -13,7 +13,7 @@ function MobileNav({open, setOpen}) {
     return (
         <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
             <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20"> {/*logo container*/}
-                <a className="text-xl font-semibold" href="/"><img src="/logo.png" width='100' height='100'   /></a>
+                <a className="text-xl font-semibold" href="/"><img src="/logo.png" width='50' height='50'   /></a>
             </div>
             <div className="flex flex-col ml-4">
                 <a className="text-xl font-medium my-4" href="/about" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
